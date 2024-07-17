352c6fda12c106e8df36b187477545a58560fe1b
Pavlo Kvas <kvaspasha@gmail.com>
2024-07-17 19:34:20 +0300

    add pause/resume; fix and refactor effect;

--- pomodoro/components/Timer.tsx
+++ pomodoro/components/Timer.tsx
@@ -1,34 +1,43 @@
 import { useEffect, useState } from "react";
-import { View, Text } from "react-native";
+import { View, Text, Button, Pressable } from "react-native";
 
 export function Timer() {
-  const [countdownTime, setCountdownTime] = useState({
-    min: 25,
-    sec: 0,
-  });
+  const [countdownTime, setCountdownTime] = useState({ min: 25, sec: 0 });
+  const [isRunning, setIsRunning] = useState(true);
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      setCountdownTime((prev) => {
-        if (prev.min === 0 && prev.sec === 0) {
-          clearInterval(interval);
-          //todo start break timer
-          return prev;
-        }
+    let interval: NodeJS.Timeout | undefined;
 
-        if (prev.sec === 0) {
-          return { min: prev.min - 1, sec: 59 };
-        }
+    if (isRunning) {
+      interval = setInterval(() => {
+        setCountdownTime((prev) => {
+          if (prev.min === 0 && prev.sec === 0) {
+            clearInterval(interval);
+            // todo: start break timer
+            return prev;
+          }
 
-        return { min: prev.min, sec: prev.sec - 1 };
-      });
-      return () => clearInterval(interval);
-    }, 1000);
-  }, []);
-  
+          const newSec = prev.sec === 0 ? 59 : prev.sec - 1;
+          const newMin = prev.sec === 0 ? prev.min - 1 : prev.min;
+
+          return { min: newMin, sec: newSec };
+        });
+      }, 1000);
+    }
+
+    return () => {
+      console.log("clearing interval");
+      clearInterval(interval);
+    };
+  }, [isRunning]);
+
   return (
     <View>
-      <Text>{countdownTime.min} : {countdownTime.sec}</Text>
+      <Pressable onPress={() => setIsRunning((prev) => !prev)}>
+        <Text>
+          {countdownTime.min} : {countdownTime.sec}
+        </Text>
+      </Pressable>
     </View>
   );
 }
